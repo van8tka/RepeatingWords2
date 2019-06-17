ab01ad2ae2220194cb4063aef9b164826b060d82
Владимирович Иван <van8tka@tut.by>
2019-06-18 00:12:13 +0300

    create unitofwork

---
+++ RepeatingWords.DataService/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using RepeatingWords.DataService.Model;
+
+namespace RepeatingWords.DataService.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<Dictionary> DictionaryRepository { get; }
+        IRepository<Words> WordsRepository { get; }
+        IRepository<LastAction> LastActionRepository { get; }
+    }
+}
---
+++ RepeatingWords.DataService/Repositories/UnitOfWork.cs
@@ -0,0 +1,23 @@
+﻿using RepeatingWords.DataService.Interfaces;
+using RepeatingWords.DataService.Model;
+
+namespace RepeatingWords.DataService.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {      
+        //ctor
+        public UnitOfWork(string dbpath)
+        {
+            _dbContext = new SQLiteContext(dbpath);
+        }
+
+        private readonly SQLiteContext _dbContext;
+
+        private IRepository<Dictionary> _dictionaryRepo;
+        public IRepository<Dictionary> DictionaryRepository => _dictionaryRepo ?? (_dictionaryRepo = new GenericRepository<Dictionary>(_dbContext));
+        private IRepository<Words> _wordsRepo;
+        public IRepository<Words> WordsRepository => _wordsRepo ?? (_wordsRepo = new GenericRepository<Words>(_dbContext));
+        private IRepository<LastAction> _lastAction;
+        public IRepository<LastAction> LastActionRepository => _lastAction ?? (_lastAction = new GenericRepository<LastAction>(_dbContext));
+    }
+}
