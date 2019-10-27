d3478042c77ca3688eb7eb63105c791723a51272
Владимирович Иван <van8tka@tut.by>
2019-10-27 11:22:29 +0300

    change button create word to toolbar button

--- RepeatingWords/RepeatingWords.Android/Resources/Resource.designer.cs
+++ RepeatingWords/RepeatingWords.Android/Resources/Resource.designer.cs
@@ -11346,28 +11346,31 @@ namespace RepeatingWords.Droid
 			public const int toolbarImage = 2131165374;
 			
 			// aapt resource value: 0x7F0700BF
-			public const int toolbarImageEdit = 2131165375;
+			public const int toolbarImageAdd = 2131165375;
 			
 			// aapt resource value: 0x7F0700C0
-			public const int toolbarImageHelp = 2131165376;
+			public const int toolbarImageEdit = 2131165376;
 			
 			// aapt resource value: 0x7F0700C1
-			public const int toolbarImageHome = 2131165377;
+			public const int toolbarImageHelp = 2131165377;
 			
 			// aapt resource value: 0x7F0700C2
-			public const int toolbarImageLike = 2131165378;
+			public const int toolbarImageHome = 2131165378;
 			
 			// aapt resource value: 0x7F0700C3
-			public const int toolbarImageTools = 2131165379;
+			public const int toolbarImageLike = 2131165379;
 			
 			// aapt resource value: 0x7F0700C4
-			public const int tooltip_frame_dark = 2131165380;
+			public const int toolbarImageTools = 2131165380;
 			
 			// aapt resource value: 0x7F0700C5
-			public const int tooltip_frame_light = 2131165381;
+			public const int tooltip_frame_dark = 2131165381;
 			
 			// aapt resource value: 0x7F0700C6
-			public const int voice = 2131165382;
+			public const int tooltip_frame_light = 2131165382;
+			
+			// aapt resource value: 0x7F0700C7
+			public const int voice = 2131165383;
 			
 			static Drawable()
 			{
