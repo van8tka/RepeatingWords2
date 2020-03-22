6361304f9670ec5e81ae4f646e11b815bb1aea5a
Владимирович Иван <van8tka@tut.by>
2020-03-22 13:41:03 +0300

    compile error fix with r8

--- RepeatingWords/RepeatingWords.Android/Properties/AssemblyInfo.cs
+++ RepeatingWords/RepeatingWords.Android/Properties/AssemblyInfo.cs
@@ -15,7 +15,7 @@ using Android.App;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-
+[assembly: UsesLibrary("org.apache.http.legacy", false)]
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
