a948daefcc2da81c45d45343808f092b3c3d52cb
Владимирович Иван <van8tka@tut.by>
2020-04-16 17:35:49 +0300

    added xamarin.esential

--- RepeatingWords/RepeatingWords/Helpers/LikeApplication.cs
+++ RepeatingWords/RepeatingWords/Helpers/LikeApplication.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Threading.Tasks;
 using RepeatingWords.Helpers.Interfaces;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace RepeatingWords.Helpers
 {
    public class LikeApplication
-    {
+   {
+       private static string uriAndroidLike = "https://play.google.com/store/apps/details?id=cardsofwords.cardsofwords";
+       //private static string uriUWPLike = "https://www.microsoft.com/store/apps/9n55bwkgshnf";
+       //private static string uriIOSLike = "";
+
         public static async Task Like(IDialogService _dialogService)
         {
             bool action = await _dialogService.ShowConfirmAsync(Resource.MessagePleaseReview, "", Resource.ButtonSendReview, Resource.ModalActCancel);
@@ -16,19 +21,22 @@ namespace RepeatingWords.Helpers
                 {
                     case Device.Android:
                     {
-                        Device.OpenUri(new Uri("https://play.google.com/store/apps/details?id=cardsofwords.cardsofwords"));
+                        if (await Launcher.CanOpenAsync(uriAndroidLike))
+                        {
+                           await Launcher.OpenAsync(uriAndroidLike);
+                        }
                         break;
                     }
-                    case Device.UWP:
-                    {
-                        Device.OpenUri(new Uri("https://www.microsoft.com/store/apps/9n55bwkgshnf"));
-                        break;
-                    }
-                    case Device.iOS:
-                    {
-                        // Device.OpenUri(new Uri("https://play.google.com/store/apps/details?id=cardsofwords.cardsofwords"));
-                        break;
-                    }
+                    //case Device.UWP:
+                    //{
+                    //    Device.OpenUri(new Uri("https://www.microsoft.com/store/apps/9n55bwkgshnf"));
+                    //    break;
+                    //}
+                    //case Device.iOS:
+                    //{
+                      
+                    //    break;
+                    //}
                 }
             }
         }
