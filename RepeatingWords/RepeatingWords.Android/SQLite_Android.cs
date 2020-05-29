3d7d66e8ea93a6241fd851bd9421f5e3c7f1ed87
Владимирович Иван <van8tka@tut.by>
2020-05-29 16:43:29 +0300

    copy old DB without version to database path

--- RepeatingWords/RepeatingWords.Android/SQLite_Android.cs
+++ RepeatingWords/RepeatingWords.Android/SQLite_Android.cs
@@ -1,15 +1,71 @@
+using System;
+using System.Diagnostics;
+using System.IO;
 using RepeatingWords.Droid;
 using Xamarin.Forms;
 using RepeatingWords.Interfaces;
+using RepeatingWords.LoggerService;
 
 [assembly: Dependency(typeof(SQLite_Android))]
 namespace RepeatingWords.Droid
 {
     public class SQLite_Android : ISQLite
     {
+        private string oldDbName = "repeatwords.db";
         public string GetDatabasePath(string filename)
         {
-            return MainActivity.Instance.GetDatabasePath(filename).AbsolutePath;
+            Log.Logger.Info("Get path to db");
+            string newPathDb = MainActivity.Instance.GetDatabasePath(filename).AbsolutePath;
+            try {
+              
+                Log.Logger.Info("Standart path to db: " + newPathDb);
+                string olderPathDb = null;
+                if (IsDbInOtherPath(oldDbName, ref olderPathDb))
+                {
+                    Log.Logger.Info("Older path to db is EXIST ");
+                    CopyToStandartAndroidDbPath(newPathDb, olderPathDb);
+                }
+                return newPathDb;
+            }
+            catch (Exception er)
+            {
+                Log.Logger.Error(er);
+                return newPathDb;
+            }
+        }
+
+        private void CopyToStandartAndroidDbPath(string newPath, string olderPath)
+        {
+            try
+            {
+                Log.Logger.Info("Copy db from older path to new path");
+                File.Move(olderPath, newPath);
+                Log.Logger.Info("Remove older db if exist");
+                if(File.Exists(olderPath))
+                    File.Delete(olderPath);
+            }
+            catch (Exception er)
+            {
+                Log.Logger.Error("Error move and delete db from older path to newpath" + er.Message);
+                throw;
+            }
+        }
+
+        private bool IsDbInOtherPath(string filename,ref string olderPathDb)
+        {
+            try
+            {
+                string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+                string path = Path.Combine(documentsPath, filename);
+                Log.Logger.Info("Older path to db: " + path);
+                olderPathDb = path;
+                return File.Exists(path);
+            }
+            catch (Exception er)
+            {
+                Log.Logger.Error("Error get older path to DB"+er.Message);
+                throw;
+            }
         }
     }
  }
