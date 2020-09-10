41067c2cc4d9a1b690b9e39c5c391de8b528d5df
Иван Кузьмук <ivan@Ivans-Mac.local>
2020-09-10 01:59:41 -0700

    added admobrenderinterstitial to ios project

---
+++ RepeatingWords/RepeatingWords.iOS/AdMobRenderInterstitial.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using RepeatingWords.iOS;
+using Xamarin.Forms;
+using Foundation;
+using Google.MobileAds;
+using UIKit;
+
+[assembly: Dependency(typeof(AdMobRendererInterstitial))]
+
+namespace RepeatingWords.iOS
+{
+    public class AdMobRendererInterstitial:IAdmobInterstitial
+    {
+        private Interstitial _interstitial; 
+        public AdMobRendererInterstitial()
+        {
+            LoadAd();
+            _interstitial.ScreenDismissed += (s, e) => LoadAd();
+        }
+
+        private void LoadAd()
+        {
+            //todo: add id adbvertizing for ios
+            _interstitial = new Interstitial("ca-app-pub-5993977371632312/9672263359");
+            var request = Request.GetDefaultRequest();
+#pragma warning disable CS0618 // Type or member is obsolete
+            request.TestDevices = new string[] { "3B68FB49-8490-48E8-BBC3-41A1BA2D41B7", "GADSimulator" };
+#pragma warning restore CS0618 // Type or member is obsolete
+            _interstitial.LoadRequest(request);
+        }
+
+        public void Show(string adUnit)
+        {
+            if(_interstitial.IsReady)
+            {
+                var view = GetVisibleViewController();
+                _interstitial.PresentFromRootViewController(view);
+            }
+        }
+
+        private UIViewController GetVisibleViewController()
+        {
+            var rootController = UIApplication.SharedApplication.KeyWindow.RootViewController;
+
+            if (rootController.PresentedViewController == null)
+                return rootController;
+
+            if (rootController.PresentedViewController is UINavigationController)
+            {
+                return ((UINavigationController)rootController.PresentedViewController).VisibleViewController;
+            }
+
+            if (rootController.PresentedViewController is UITabBarController)
+            {
+                return ((UITabBarController)rootController.PresentedViewController).SelectedViewController;
+            }
+
+            return rootController.PresentedViewController;
+        }
+    }
+}
--- RepeatingWords/RepeatingWords.iOS/SQLite_iOS.cs
+++ RepeatingWords/RepeatingWords.iOS/SQLite_iOS.cs
@@ -1,20 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Foundation;
 using RepeatingWords.Interfaces;
 using RepeatingWords.iOS;
-using UIKit;
 using Xamarin.Forms;
-using RepeatingWords.Interfaces;
 using RepeatingWords.LoggerService;
 using System.IO;
-[assembly:Dependency(typeof(SQLite_iOS))]
+[assembly: Dependency(typeof(SQLite_iOS))]
 namespace RepeatingWords.iOS
 {
-   public class SQLite_iOS:ISQLite
+    public class SQLite_iOS:ISQLite
     {
         public string GetDatabasePath(string filename)
         {
